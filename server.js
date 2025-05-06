353c1bbc08931e72f55bfcebdb600d268dfbe47a
iAyomi <ayomitunde.isijola@gmail.com>
2025-05-06 17:32:34 +0100

    feat: Re-implemented server side logic without nanoid package

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 import express from "express";
 import cors from "cors";
-import { nanoid } from "nanoid";
+import crypto from "crypto";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const urlMap = {};
+const urlDataByHash = {};
+const urlDataByShortURLPath = {};
+
+const hashLongURL = (longURL) => {
+  const hash = crypto.createHash("sha256");
+  hash.update(longURL);
+  return hash.digest("hex");
+};
+
+const truncateHash = (hash, store, truncVal = 6) => {
+  const truncatedHash = hash.slice(0, truncVal);
+  if (store[truncatedHash]) {
+    return truncateHash(hash, store, truncVal + 1);
+  }
+  return truncatedHash;
+};
 
 app.use(cors());
 app.use(express.json());
@@ -18,36 +33,42 @@ app.get("/", (req, res) => {
 app.get("/api/statistic/:shortURLpath", (req, res) => {
   const { shortURLpath } = req.params;
 
-  if (!shortURLpath) {
-    return res.status(400).json({ error: "Short URL path is required" });
+  const data = urlDataByShortURLPath[shortURLpath];
+
+  if (!data) {
+    return res.status(404).json({ error: "URL not found" });
   }
 
-  const foundObjWithShortURLPath = Object.entries(urlMap).find(
-    ([_, data]) => data.shortURLpath === shortURLpath
-  );
+  const { longURL } = data;
+  const hashedLongURL = hashLongURL(longURL);
 
-  if (foundObjWithShortURLPath) {
-    return res.json({
-      longURL: foundObjWithShortURLPath[1].longURL,
-      dateCreated: foundObjWithShortURLPath[1].dateCreated,
-      accessCount: foundObjWithShortURLPath[1].accessCount,
-    });
-  }
-
-  res.status(404).json({ error: "Short URL not found" });
+  return res.json({
+    longURL,
+    dateCreated: urlDataByHash[hashedLongURL].dateCreated,
+    accessCount: urlDataByHash[hashedLongURL].accessCount,
+    lastTimeAccessed: urlDataByHash[hashedLongURL].lastTimeAccessed,
+  });
 });
 
 // Get all URL list
 app.get("/api/list", (req, res) => {
   const query = req.query.search?.toLowerCase() || "";
 
-  const filteredURLs = Object.entries(urlMap)
-    .filter(([longURL, data]) => {
-      return longURL.toLowerCase().includes(query);
-    })
-    .map(([longURL, data]) => ({
-      ...data,
-    }));
+  const filteredURLs = Object.entries(urlDataByShortURLPath)
+    .filter(([_, data]) => data.longURL.toLowerCase().includes(query))
+    .map(([shortURLpath, data]) => {
+      const hashedLongURL = hashLongURL(data.longURL);
+      const meta = urlDataByHash[hashedLongURL] || {};
+
+      return {
+        id: shortURLpath,
+        shortURL: meta.shortURL,
+        longURL: data.longURL,
+        dateCreated: meta.dateCreated,
+        accessCount: meta.accessCount,
+        lastTimeAccessed: meta.lastTimeAccessed,
+      };
+    });
 
   res.json({ filteredURLs });
 });
@@ -60,67 +81,72 @@ app.post("/api/encode", (req, res) => {
     return res.status(400).json({ error: "URL is required" });
   }
 
-  if (urlMap[longURL]) {
+  const hashedLongURL = hashLongURL(longURL);
+
+  if (urlDataByHash[hashedLongURL]) {
     return res.json({
       longURL,
-      shortURL: urlMap[longURL].shortURL,
-      message: "URL has already been encoded",
+      shortURL: urlDataByHash[hashedLongURL].shortURL,
+      message: "URL has already been encoded!",
     });
   }
 
-  const shortURLpath = nanoid(6);
+  const shortURLpath = truncateHash(hashedLongURL, urlDataByShortURLPath);
   const shortURL = `http://localhost:${PORT}/${shortURLpath}`;
   const dateCreated = new Date().toUTCString();
   const accessCount = 0;
+  const lastTimeAccessed = null;
 
-  urlMap[longURL] = {
-    shortURLpath,
-    longURL,
+  urlDataByHash[hashedLongURL] = {
     shortURL,
     dateCreated,
     accessCount,
+    lastTimeAccessed,
   };
 
-  res.json({ longURL, shortURL });
+  urlDataByShortURLPath[shortURLpath] = {
+    longURL,
+  };
+
+  res.json({ longURL, shortURL, message: "URL encoded successfully!" });
 });
 
 // Decode a URL
 app.post("/api/decode", (req, res) => {
   const { shortURLpath } = req.body;
 
-  if (!shortURLpath) {
-    return res.status(400).json({ error: "Short URL path is required" });
+  const data = urlDataByShortURLPath[shortURLpath];
+
+  if (!data) {
+    return res.status(404).json({ error: "URL not found" });
   }
 
-  const foundObjWithShortURLPath = Object.entries(urlMap).find(
-    ([_, data]) => data.shortURLpath === shortURLpath
-  );
-
-  if (foundObjWithShortURLPath) {
-    return res.json({ longURL: foundObjWithShortURLPath[1].longURL });
-  }
-
-  res.status(404).json({ error: "Short URL not found" });
+  return res.json({
+    longURL: data.longURL,
+    message: "URL decoded successfully!",
+  });
 });
 
 // Redirect shortURL => longURL
 app.get("/:shortURLpath", (req, res) => {
   const { shortURLpath } = req.params;
 
-  if (!shortURLpath) {
-    return res.status(400).json({ error: "Short URL path is required" });
+  const data = urlDataByShortURLPath[shortURLpath];
+
+  if (!data) {
+    return res.status(404).json({ error: "URL not found" });
   }
 
-  const foundObjWithShortURLPath = Object.entries(urlMap).find(
-    ([_, data]) => data.shortURLpath === shortURLpath
-  );
+  const { longURL } = data;
+  const hashedLongURL = hashLongURL(longURL);
 
-  if (foundObjWithShortURLPath) {
-    foundObjWithShortURLPath[1].accessCount += 1;
-    return res.redirect(foundObjWithShortURLPath[1].longURL);
-  }
+  urlDataByHash[hashedLongURL] = {
+    ...urlDataByHash[hashedLongURL],
+    accessCount: (urlDataByHash[hashedLongURL].accessCount || 0) + 1,
+    lastTimeAccessed: new Date().toUTCString(),
+  };
 
-  res.status(404).json({ error: "URL not found" });
+  return res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
