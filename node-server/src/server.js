4dfcb780f89c3dd67dc54cb97211e84dac25bcf9
iAyomi <ayomitunde.isijola@gmail.com>
2025-05-07 20:32:16 +0100

    changes: Server Index Page text content changes

--- node-server/src/server.js
+++ node-server/src/server.js
@@ -20,17 +20,9 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-  // Application specific logging, throwing an error, or other logic here
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught Exception:", err);
-  process.exit(1); // Exit the process with a failure code
+  res.send(
+    "Welcome to the URL Shortener Service! Visit /api-docs for API documentation."
+  );
 });
 
 app.listen(PORT, () => {
