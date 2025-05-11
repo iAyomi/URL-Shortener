5cd4e62dd3674f3e1f01a0d4651e3b932fad30fd
iAyomi <ayomitunde.isijola@gmail.com>
2025-05-11 22:40:46 +0100

    new feat🎉: To implement findAlternateShortURLs using an LLM

---
+++ node-server/src/utils/findAlternateShortURLs.util.js
@@ -0,0 +1,13 @@
+const findAlternateShortURLs = () => {
+  const alternatives = [
+    { id: "1", name: "abcd" },
+    { id: "2", name: "eeew" },
+    { id: "3", name: "rfd" },
+    { id: "4", name: "scv" },
+    { id: "5", name: "fd.v" },
+    { id: "6", name: "rt.o" },
+  ];
+  return alternatives;
+};
+
+export default findAlternateShortURLs;
