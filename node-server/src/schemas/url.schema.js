086e00949066374027b8fbb76cacc6a32843f2b8
iAyomi <ayomitunde.isijola@gmail.com>
2025-06-20 16:52:03 +0100

    changes🛠: Improved Url Test Schema to use isValidURL util

--- node-server/src/schemas/url.schema.js
+++ node-server/src/schemas/url.schema.js
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import isValidURL from "../utils/isValidURL.util.js";
 
 // export const getURLStatsSchema = yup.object({
 //   shortURLpath: yup.string().required("Short URL is required"),
@@ -9,12 +10,18 @@ export const getAllURLsListSchema = yup.object({
 });
 
 export const encodeSchema = yup.object({
-  longURL: yup.string().url("Invalid URL format").required("URL is required"),
+  longURL: yup
+    .string()
+    .test("is-url-valid", "Invalid URL format", (value) => isValidURL(value))
+    .required("URL is required"),
   customUrl: yup.string().max(7, "Custom URL must be at most 7 characters"),
 });
 
 export const decodeSchema = yup.object({
-  shortURL: yup.string().url("Invalid URL format").required("URL is required"),
+  shortURL: yup
+    .string()
+    .test("is-url-valid", "Invalid URL format", (value) => isValidURL(value))
+    .required("URL is required"),
 });
 
 // export const redirectSchema = yup.object({
